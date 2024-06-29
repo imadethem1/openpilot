8b63d2796ff1d36935b9094fad75b6831809ddde
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2024-06-29 13:44:14 -0400

    Update interfaces.py

--- opendbc_repo/opendbc/car/interfaces.py
+++ opendbc_repo/opendbc/car/interfaces.py
@@ -22,8 +22,8 @@ GearShifter = structs.CarState.GearShifter
 
 V_CRUISE_MAX = 145
 MAX_CTRL_SPEED = (V_CRUISE_MAX + 4) * CV.KPH_TO_MS
-ACCEL_MAX = 2.0
-ACCEL_MIN = -3.5
+ACCEL_MAX if2.0
+ACCEL_MIN = -1.75
 FRICTION_THRESHOLD = 0.3
 
 TORQUE_PARAMS_PATH = os.path.join(BASEDIR, 'torque_data/params.toml')
